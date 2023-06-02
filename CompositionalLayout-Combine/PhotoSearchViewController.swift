c7313aa95b5c31bc12aee2c58031402b2b1a1774
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-06-02 19:42:15 -0400

    configured app programmatically

--- CompositionalLayout-Combine/PhotoSearchViewController.swift
+++ CompositionalLayout-Combine/PhotoSearchViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotoSearchViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue.withAlphaComponent(0.8)
     }
-
-
 }
